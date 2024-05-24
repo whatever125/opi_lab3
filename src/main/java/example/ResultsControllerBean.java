5af70c87072a737525aafd490b62f4023025b559
bozhenka <bozhena.andreeva@gmail.com>
2024-05-24 23:28:58 +0300

    add javadoc

--- src/main/java/example/ResultsControllerBean.java
+++ src/main/java/example/ResultsControllerBean.java
@@ -21,22 +21,46 @@ import java.io.Serializable;
 /**
  * Managed bean for handling results in JSF application.
  * This bean is responsible for managing operations related to result entities.
+ * @author bozhenka
  */
 @Named
 @ApplicationScoped
 @Data
 @Slf4j
 public class ResultsControllerBean implements Serializable {
+    /**
+     * Injected dependency on XBean.
+     */
     @Inject
     private XBean xBean;
+
+    /**
+     * Injected dependency on YBean.
+     */
     @Inject
     private YBean yBean;
+
+    /**
+     * Injected dependency on RBean.
+     */
     @Inject
     private RBean rBean;
 
+    /**
+     * Data Access Object for ResultEntity.
+     */
     private ResultDAO resultDAO;
+
+    /**
+     * List of result entities.
+     */
     private ArrayList<ResultEntity> results = new ArrayList<>();
 
+    /**
+     * Initializes the results controller bean by fetching all results from the database
+     * and storing them in the results list. This method is called after the bean's
+     * properties have been injected.
+     */
     @PostConstruct
     public void init() {
         resultDAO = DAOFactory.getInstance().getResultDAO();
@@ -46,6 +70,13 @@ public class ResultsControllerBean implements Serializable {
         log.info("Results initialized with {} entries.", results.size());
     }
 
+    /**
+     * Adds a new result to the list of results and stores it in the database.
+     *
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     * @param r the R radius
+     */
     public void addResult(Double x, Double y, Double r) {
         final long startTime = System.nanoTime();
 
@@ -69,6 +100,9 @@ public class ResultsControllerBean implements Serializable {
         FacesContext.getCurrentInstance().getPartialViewContext().getEvalScripts().add(script2);
     }
 
+    /**
+     * Draws the results on the client side by sending JavaScript commands.
+     */
     public void drawResults() {
         for (ResultEntity result: getSessionResults()) {
             String script1 = String.format(Locale.US, "window.drawResult(%f, %f, %f, %b);", result.getX(), result.getY(), result.getR(), result.isSuccess());
@@ -76,6 +110,11 @@ public class ResultsControllerBean implements Serializable {
         }
     }
 
+    /**
+     * Retrieves the results associated with the current session.
+     *
+     * @return a list of result entities associated with the current session
+     */
     public List<ResultEntity> getSessionResults() {
         FacesContext fCtx = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) fCtx.getExternalContext().getSession(false);
@@ -86,6 +125,9 @@ public class ResultsControllerBean implements Serializable {
                 .toList();
     }
 
+    /**
+     * Clears the results associated with the current session from the list and the database.
+     */
     public void clearSessionResults() {
         FacesContext fCtx = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) fCtx.getExternalContext().getSession(false);
@@ -98,6 +140,9 @@ public class ResultsControllerBean implements Serializable {
         FacesContext.getCurrentInstance().getPartialViewContext().getEvalScripts().add(script1);
     }
 
+    /**
+     * Updates the canvas on the client side to reflect the current state.
+     */
     public void updateCanvas() {
         String script1 = String.format(Locale.US, "window.updateCanvas(%f);", rBean.getR());
         FacesContext.getCurrentInstance().getPartialViewContext().getEvalScripts().add(script1);
