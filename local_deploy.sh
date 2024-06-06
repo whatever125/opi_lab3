367d4ef0821a9237ee5b4b47487ed50dcb67be8a
Dmitriy Kolmakov <kolmakov125@gmail.com>
2024-06-06 23:48:42 +0300

    add deploy file

--- local_deploy.sh
+++ local_deploy.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-echo "Deploying local"
+echo "Starting deployment..."
+
+WAR_NAME="LAB3.war"
+WILDFLY_LOCATION="$HOME/Development/wildfly-preview-26.1.0.Final"
 
 # Build war
 ant build
 # Remove existing deployment
-rm ~/Development/wildfly-preview-26.1.3.Final/standalone/deployments/LAB3.war
+rm -f "$WILDFLY_LOCATION"/standalone/deployments/"$WAR_NAME"*
 # Add new deployment
-cp ./build/LAB3.war ~/Development/wildfly-preview-26.1.3.Final/standalone/deployments/LAB3.war
+cp ./build/"$WAR_NAME" "$WILDFLY_LOCATION"/standalone/deployments/"$WAR_NAME"
+# Run wildfly
+"$WILDFLY_LOCATION"/bin/standalone.sh \
+-b 0.0.0.0 \
+-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Dcom.sun.management.jmxremote.port=7203 \
+-Dcom.sun.management.jmxremote.rmi.port=7203 \
+-Djava.rmi.server.hostname=0.0.0.0 \
+-Djboss.bind.address.management=0.0.0.0 \
+-Djboss.bind.address=0.0.0.0
