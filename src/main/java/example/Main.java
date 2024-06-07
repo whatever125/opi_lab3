e9436d33d299d67ead54c8e345b2b56372ba0633
bozhenka <bozhena.andreeva@gmail.com>
2024-06-07 12:36:45 +0300

    revert main

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,17 +1,7 @@
 package example;
 
-import example.mbeans.NumberOfPoints;
-
-import javax.management.*;
-import java.lang.management.ManagementFactory;
-
 public class Main {
-    public static void main(String[] args) throws NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException, MalformedObjectNameException {
-        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        ObjectName name = new ObjectName("example.mbeans:type=NumberOfPoints");
-        NumberOfPoints mbean = new NumberOfPoints();
-        mbs.registerMBean(mbean, name);
-
+    public static void main(String[] args) {
         System.out.println("Successfully launched Main!");
     }
 }
